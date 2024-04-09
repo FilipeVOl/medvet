d588df1f1e1e145f25d213811dfdea76d7b27ad4
filipe gideao <filipegrodriguesreal@gmail.com>
2024-04-09 17:44:15 -0300

    FIX: Finalização de estilização

--- src/Component/Cadastro.jsx
+++ src/Component/Cadastro.jsx
@@ -11,42 +11,43 @@ export default function Cadastro() {
         <form>
         <div className="forms-container px-28 grid w-full justify-center">
           <div className="box-1 grid grid-cols-[2fr_1fr] gap-8">
-            <label htmlFor="nome" className="font-Montserrat indent-4">Nome completo<br></br>
-                <input type="text"
-                className="w-full border-2 border-border-gray rounded-md h-9"/> </label>
+            <label htmlFor="nome" className="font-Montserrat indent-4">Nome completo *<br></br>
+                <input type="text" required
+                className="w-full border-[1px] border-border-gray rounded-md h-9 pl-2"/> </label>
 
-            <label htmlFor="matricula" className="flex flex-col">Matricula<br></br>
-                <input type="text"
-                className="border-2 w-[300px] border-border-gray rounded-md h-9"/> </label>
+            <label htmlFor="matricula" className="flex flex-col indent-4">Matricula<br></br>
+                <input type="text" required
+                className="border-[1px] w-[300px] border-border-gray rounded-md h-9 pl-2"/> </label>
           </div>
 
           <div className="box-2 grid grid-cols-[196px_400px] gap-32">
-            <label htmlFor="cpf" className="font-Montserrat">CPF<br></br>
-                <InputMask mask="999.999.999-99" className="border-2 w-64 border-border-gray rounded-md h-9" />
-            </label>    
-            <label htmlFor="email" className="font-Montserrat">Email<br></br>
-                <input type="email"
-                className="w-[590px] border-2 border-border-gray rounded-md h-9"/> </label>
+            <label htmlFor="cpf" className="font-Montserrat indent-4">CPF<br></br>
+                <InputMask mask="999.999.999-99" required className="border-[1px] w-64 border-border-gray rounded-md h-9 pl-2" />
+            </label>
+
+            <label htmlFor="email" className="font-Montserrat indent-4">Email<br></br>
+                <input type="email" required
+                className="w-[590px] border-[1px] border-border-gray rounded-md h-9 pl-2"/> </label>
           </div>
 
           <div className="box-3 grid grid-cols-[200px_390px] gap-44">
-            <label htmlFor="telefone" className="font-Montserrat">N° de telefone<br></br>
-                <input type="tel"
-                className="border-2 w-72 border-border-gray rounded-md h-9"/> </label>
+            <label htmlFor="telefone" className="font-Montserrat indent-4">N° de telefone<br></br>
+            <InputMask mask="(99)9999-99999" required className="border-[1px] w-72 border-border-gray rounded-md h-9 pl-2" />
+            </label>
 
             <label htmlFor="curso">Curso<br></br>
-                <input type="text"
-                className="w-[539px] border-2 border-border-gray rounded-md h-9"/> </label>
+                <input type="text" required
+                className="w-[539px] border-[1px] border-border-gray rounded-md h-9 indent-4 pl-2"/> </label>
           </div>
 
           <div className="box-4 grid grid-cols-[200px_200px] gap-8">
-            <label htmlFor="periodo" className="font-Montserrat">Período<br></br>
-                <input type="text"
-                className="border-2 border-border-gray rounded-md h-9"/> </label>
+            <label htmlFor="periodo" className="font-Montserrat indent-4">Período<br></br>
+            <input type="number" required className="border-[1px] border-border-gray rounded-md h-9 pl-2" maxLength={2} />
+            </label>
 
-            <label htmlFor="turno" className="font-Montserrat">Turno<br></br>
-                <input type="text"
-                className="border-2 border-border-gray rounded-md h-9"/> </label>
+            <label htmlFor="turno" className="font-Montserrat indent-4">Turno<br></br>
+                <input type="text" required
+                className="border-[1px] border-border-gray rounded-md h-9 pl-2"/> </label>
           </div>
           </div>
 
