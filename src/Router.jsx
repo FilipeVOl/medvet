c0ff893f78aa5c333782ea0b51be9255db6d1137
verezeeee <theusma58@gmail.com>
2024-04-13 02:06:33 -0300

    Corrigir formatação

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,14 +1,13 @@
-import { Routes, Route} from "react-router-dom"
-import Cadastro from "./Component/Cadastro"
+import { Routes, Route } from "react-router-dom";
+import Cadastro from "./Component/Cadastro";
 import { DefaultLayout } from "./layouts/DefaultLayout";
 
-
 export function Router() {
-    return (
-      <Routes>
-        <Route path="/" element={<DefaultLayout/>}>
-          <Route path="cadastro" element={<Cadastro />} />
-          </Route>
-      </Routes>
-    );
-  }
+  return (
+    <Routes>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="cadastro" element={<Cadastro />} />
+      </Route>
+    </Routes>
+  );
+}
