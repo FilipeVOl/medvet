27873d4bd8ae7330261a321c2856859f74f6e681
verezeeee <theusma58@gmail.com>
2024-04-13 01:24:29 -0300

    Adiciona dependência axios ao package.json

--- src/Component/TelaConsulta.jsx
+++ src/Component/TelaConsulta.jsx
@@ -1,64 +1,53 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
+import { Input, InputLabel } from "@mui/material";
 import PropTypes from "prop-types";
 import z from "zod";
+import axios from "axios";
 
 const schema = z.object({
   paciente: z.string(),
   tutor: z.string(),
   data: z.string(),
-  hora: z.string(),
+  especie: z.string(),
   contato: z.string(),
   obs: z.string(),
 });
 
 const TelaConsulta = () => {
-  const [pacientes, setPacientes] = useState([]);
-  const [tutores, setTutores] = useState([]);
+  const [pacientes, setPacientes] = useState("");
+  const [tutores, setTutores] = useState("");
   const [data, setData] = useState("");
-  const [hora, setHora] = useState("");
+  const [especie, setEspecie] = useState("");
   const [contato, setContato] = useState("");
   const [obs, setObs] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = () => {
     const info = {
       paciente: pacientes,
       tutor: tutores,
-      data,
-      hora,
-      contato,
-      obs,
+      data: data,
+      especie: especie,
+      contato: contato,
+      obs: obs,
     };
     schema.parse(info);
     console.log(info);
   };
 
-  const NormalInput = ({ label, type, onchange, value }) => {
-    return (
-      <div className="w-full flex flex-col mb-4">
-        <label className="ml-4">{label}</label>
-        <input
-          onChange={(e) => {
-            onchange(e.target.value);
-          }}
-          type={type}
-          value={value}
-          className={` h-[46px] rounded-[10px] p-2 border border-[#838383] `}
-        />
-      </div>
-    );
-  };
+  const handleChange = useCallback((value, setter) => {
+    setter(value);
+  }, []);
 
   const Dropdown = ({ label, options, onchange, value }) => {
     return (
       <div className="flex flex-col w-full mb-4">
-        <label className="ml-4">{label}</label>
+        <InputLabel className="ml-4">{label}</InputLabel>
         <select
           onChange={(e) => {
             onchange(e.target.value);
           }}
           value={value}
-          className="w-full h-[46px] rounded-[10px] p-2 border border-[#838383] bg-white"
+          className="w-full h-[46px] rounded-[2px] p-2 border border-[#838383] bg-white"
         >
           {options.map((option) => (
             <option key={option} value={option.value}>
@@ -87,7 +76,7 @@ const TelaConsulta = () => {
               options={dataPacientes}
               value={pacientes}
               onchange={(value) => {
-                setPacientes(value);
+                handleChange(value, setPacientes);
               }}
             />
             <Dropdown
@@ -95,45 +84,69 @@ const TelaConsulta = () => {
               options={dataPacientes}
               value={tutores}
               onchange={(value) => {
-                setTutores(value);
+                handleChange(value, setTutores);
               }}
             />
           </div>
-          <div className="flex w-full gap-8">
-            <NormalInput
-              label="Data"
-              type="date"
-              value={data}
-              onchange={(value) => {
-                setData(value);
-              }}
-            />
-            <NormalInput
-              label="Hora"
-              type="time"
-              value={hora}
-              onchange={(value) => {
-                setHora(value);
-              }}
-            />
-            <NormalInput
-              label="Contato"
-              type="text"
-              value={contato}
-              onchange={(value) => {
-                setContato(value);
-              }}
-            />
+          <div className="flex w-full gap-8 justify-between mb-8">
+            <div className="flex flex-col w-1/3">
+              <InputLabel className="ml-4" htmlFor="especie">
+                Especie
+              </InputLabel>
+              <Input
+                label="Especie"
+                type="text"
+                value={especie}
+                onChange={(e) => {
+                  handleChange(e.target.value, setEspecie);
+                }}
+                className={`border ${especie === "" ? "border-red-600" : " border-[#848484]"
+                  } rounded-[2px] h-[46px] p-2`}
+              />
+            </div>
+
+            <div className="flex flex-col w-1/3">
+              <InputLabel className="ml-4" htmlFor="contato">
+                Contato
+              </InputLabel>
+              <Input
+                label="Contato"
+                type="text"
+                value={contato}
+                onChange={(e) => {
+                  handleChange(e.target.value, setContato);
+                }}
+                className={`border ${contato === "" ? "border-red-600" : " border-[#848484]"
+                  } rounded-[2px] h-[46px] p-2`}
+              />
+            </div>
+
+            <div className="flex flex-col w-1/3">
+              <InputLabel className="ml-4 text-black" htmlFor="data">
+                Data
+              </InputLabel>
+              <Input
+                label="Data"
+                type="text"
+                value={data}
+                onChange={(e) => {
+                  handleChange(e.target.value, setData);
+                }}
+                className={`border ${data === "" ? "border-red-600" : " border-[#848484]"
+                  } rounded-[2px] h-[46px] p-2`}
+              />
+            </div>
           </div>
           <label className="ml-4" htmlFor="obs">
-            Observação
+            Observação*
           </label>
           <textarea
             value={obs}
             onChange={(e) => {
-              setObs(e.target.value);
+              handleChange(e.target.value, setObs);
             }}
-            className="border border-[#848484] rounded-[10px] h-[92px] p-2 text-base w-full resize-none"
+            className={`border ${obs === "" ? "border-red-600" : "border-[#848484]"
+              }  rounded-[2px] h-[92px] p-2 text-base w-full resize-none`}
           />
         </form>
         <div className="justify-between flex ml-4 mt-10">
