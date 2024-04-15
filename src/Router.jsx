9c42eedfd9adb99e812c34ca51cb860f37f0f79a
GusTavoTaVaresB <gustavohenriquetavaresborges@gmail.com>
2024-04-15 15:27:06 -0300

    telaProfessor

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Cadastro from "./pages/Cadastro";
 import Agendamento from "./pages/Agendamento";
+import Professor from "./pages/professor";
 import { DefaultLayout } from "./layouts/DefaultLayout";
 
 export function Router() {
@@ -9,6 +10,7 @@ export function Router() {
       <Route path="/" element={<DefaultLayout/>}>
         <Route path="cadastro" element={<Cadastro />} />
         <Route path="agendamento" element={<Agendamento />} />
+        <Route path="professor" element={<Professor />} />
       </Route>
     </Routes>
   );
