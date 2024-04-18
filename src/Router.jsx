a9e5e1051cabacc25da0bd53c88a4fdb31887a59
filipe gideao <filipegrodriguesreal@gmail.com>
2024-04-18 15:37:54 -0300

    MERGE: Cadastro Atualizado

---
+++ src/Router.jsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import Cadastro from "./pages/Cadastro";
+import Agendamento from "./pages/Agendamento";
+import Professor from "./pages/professor";
+import { DefaultLayout } from "./layouts/DefaultLayout";
+import Agenda from "./pages/Agenda";
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<DefaultLayout/>}>
+        <Route path="cadastro" element={<Cadastro />} />
+        <Route path="agendamento" element={<Agendamento />} />
+        <Route path="professor" element={<Professor />} />
+        <Route path="agenda" element={<Agenda/>} />
+      </Route>
+    </Routes>
+  );
+}
