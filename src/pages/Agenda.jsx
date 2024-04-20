a848ae33048071361ec150bddd1a7d6f8dd02559
Fernando Oliveira <primoflro@gmail.com>
2024-04-20 14:21:03 -0300

    Buscar por Nome do Animal ou do Tutor

--- src/pages/Agenda.jsx
+++ src/pages/Agenda.jsx
@@ -4,18 +4,32 @@ import axios from 'axios';
 
 export default function Agenda() {
   const [agenda, setAgenda] = useState([]);
-
+  const [alteredAgenda, setAlteredAgenda] = useState([]);
+  const [nome, setNome] = useState('');
   useEffect(() => {
     axios.get('http://localhost:3333/get/consults')
       .then(response => {
         setAgenda(response.data);
+        setAlteredAgenda(response.data);
       })
       .catch(error => {
         console.error('Error fetching data:', error);
       });
   }, []);
-
-
+  const consultasFiltradas = (() => {
+    const dateAlt = Object.keys(agenda).reduce((acc, key) => {
+      const consultasDoDia = agenda[key].filter(
+        consulta =>
+          consulta.nameTutor.toLowerCase().includes(nome.toLowerCase()) ||
+          consulta.nameAnimal.toLowerCase().includes(nome.toLowerCase())
+      );
+      if (consultasDoDia.length > 0) {
+        acc[key] = consultasDoDia;
+      }
+      return acc;
+    }, {})
+    return Object.keys(dateAlt).length == 0 ? agenda : dateAlt
+  });
   function transData(dateString) {
     const day = dateString.toString().substring(0, 2);
     const month = dateString.toString().substring(2, 4);
@@ -33,15 +47,23 @@ export default function Agenda() {
           <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
               <img src={iconSearch}/>
           </div>
-          <input type="text" id="simple-search" className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 w-3/12" placeholder="Buscar por data" required />
+          <input type="text" id="simple-search" className=" bg-gray-50 border border-gray-300
+        text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 
+          block ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400
+        dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 w-3/12" 
+          placeholder="Buscar por Nome" required 
+          onChange={(e) => {
+            setNome(e.target.value);
+          {e.target.value.length == 0 ? setAlteredAgenda(agenda) : setAlteredAgenda(consultasFiltradas)}
+          }}/>
       </section>
       </section>
       <section className='mx-24'>
-          {Object.keys(agenda).map((dados) => {
+          {Object.keys(alteredAgenda).map((dados) => {
             return (
               <div key = {dados} className="m-12 pr-40 max-w-4xl">
                 <h2 className="text-2xl pr-0 text-text-gray font-semibold">{transData(dados)}</h2>
-                {agenda[dados].map((e) => {
+                {alteredAgenda[dados].map((e) => {
                   return (
                     <div key={e}className='flex bg-side-gray my-4 rounded-lg ml-4'>
                       <div className='bg-card-green m-0 text-transparent  rounded-lg'>a</div>
