5cd574bbb4ee869f142439cdd6ebbd55576a910c
Jeniffer Ferraz Pires <ferrazjeniffer@hotmail.com>
2025-03-04 23:12:50 -0300

    Fix: arrumando em tese a sidebar (embora nao funciona o fixed, pq se nao teriamos que arrumar todos os componentes

--- src/Component/Accordion.jsx
+++ src/Component/Accordion.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import arrow from '../images/arrow.svg'
-import novoCadastro from '../images/novo-cadastro.svg'
 import { Link } from 'react-router-dom';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
 const Accordion = (props) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,20 +12,34 @@ const Accordion = (props) => {
   const question = props.options;
   const values = Object.values(question);
   const keys = Object.keys(question);
+
   return (
     <div className='transition ease-in-out w-full'>
-      <div className='flex gap-4 align-middle justify-between items-center cursor-pointer' onClick={toggleAccordion}>
+      <div className='flex gap-4 align-middle justify-between items-center cursor-pointer py-4 px-2 hover:bg-[#008854] rounded-md transition-all duration-300' 
+           onClick={toggleAccordion}>
         <div className='flex items-center gap-4'>
-          <img src={props.image} className='self-start my-4' />
+          {props.icon}
           <span className='justify-self-start'>{props.text}</span>
         </div>
-        <img src={arrow} className={`${isOpen ? 'rotate-180' : ''} transition ease-in-out duration-500`} />
+        {isOpen ? (
+          <KeyboardArrowUpIcon className='transition ease-in-out duration-500' />
+        ) : (
+          <KeyboardArrowDownIcon className='transition ease-in-out duration-500' />
+        )}
       </div>
-      <div className={` ${isOpen ? 'max-h-96 opacity-100' : 'max-h-0 opacity-0'
-        } overflow-hidden flex gap-2 flex-col self-end ml-9`}
+      <div 
+        className={`${
+          isOpen ? 'max-h-96 opacity-100 py-2' : 'max-h-0 opacity-0'
+        } overflow-hidden flex gap-3 flex-col self-end ml-9 transition-all duration-300`}
       >
         {values.map((option, index) => (
-          <Link key={index} to={option} className='cursor-pointer'>{keys[index]}</Link>
+          <Link 
+            key={index} 
+            to={option} 
+            className='cursor-pointer hover:bg-[#008854] py-2 px-2 rounded-md transition-all duration-300'
+          >
+            {keys[index]}
+          </Link>
         ))}
       </div>
     </div>
--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,49 +1,48 @@
-import arrow from '../images/arrow.svg'
-import house from '../images/house.svg'
-import agendar from '../images/agendar.svg'
-import novoCadastro from '../images/novo-cadastro.svg'
-import agendamento from '../images/agendamento.svg'
-import cadastrados from '../images/cadastrados.svg'
-import prontuarios from '../images/prontuarios.svg'
 import { Link } from "react-router-dom";
-import Accordion from './Accordion'
+import Accordion from './Accordion';
+import HomeIcon from '@mui/icons-material/Home';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import EventAvailableIcon from '@mui/icons-material/EventAvailable';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import MedicalServicesIcon from '@mui/icons-material/MedicalServices';
+import ReceiptIcon from '@mui/icons-material/Receipt';
+import PeopleIcon from '@mui/icons-material/People';
+import FolderSharedIcon from '@mui/icons-material/FolderShared';
 
 const Navbar = () => {
   const arr = {Aluno: 'aluno', Tutor: 'tutor', Professor: 'professor', Animal: 'animal'};
   const arr2 = {Aluno: 'showAluno', Tutor: 'showTutor', Professor: 'showProfessor'};
   return (
     <div className='shadow-md min-h-screen bg-[#007448] p-10 text-white-med top-28 max-w-72 min-w-72'>
-      <div id="inicio" className='flex py-4'>
-        <img src={house} className='pr-4 '/>
-        <Link to="/">Início</Link>
+      <div id="inicio" className='flex py-4 hover:bg-[#008854] rounded-md px-2 transition-all duration-300'>
+        <HomeIcon className='mr-4' />
+        <Link to="/" className="w-full">Início</Link>
       </div>
-      <div id="agendar" className='flex py-4'>
-        <img src={agendar} className='pr-4 h-6'/>
-        <Link to="agendamento">Agendar</Link>
+      <div id="agendar" className='flex py-4 hover:bg-[#008854] rounded-md px-2 transition-all duration-300'>
+        <CalendarMonthIcon className='mr-4' />
+        <Link to="agendamento" className="w-full">Agendar</Link>
       </div>
-      <div id="novo-cadastro" className='flex bg-[#007448]'>
+      <div id="agendamento" className='flex py-4 hover:bg-[#008854] rounded-md px-2 transition-all duration-300'>
+        <EventAvailableIcon className='mr-4' />
+        <Link to="agenda" className="w-full">Agendamento</Link>
       </div>
-      <div id="agendamento" className='flex py-4'>
-        <img src={agendamento} className='pr-4'/>
-        <Link to="agenda">Agendamento</Link>
+      <div className="my-2">
+        <Accordion text="Novo Cadastro" icon={<PersonAddIcon />} options={arr}/>
       </div>
-      <div>
-        <Accordion text="Novo Cadastro" image={novoCadastro} options={arr}/>
+      <div id="criarConsulta" className='flex py-4 hover:bg-[#008854] rounded-md px-2 transition-all duration-300'>
+        <MedicalServicesIcon className='mr-4' />
+        <Link to="criarconsulta" className="w-full">Criar Consulta</Link>
       </div>
-      <div id="criarConsulta" className='flex py-4 bg-[#007448]'>
-        <img src={agendar} alt='criar consulta' className='pr-4 h-6'/>
-        <Link to="criarconsulta">Criar Consulta</Link>
+      <div id='receita' className='flex py-4 hover:bg-[#008854] rounded-md px-2 transition-all duration-300'>
+        <ReceiptIcon className='mr-4' />
+        <Link to="receita" className="w-full">Receitas</Link>
       </div>
-      <div id='receita' className='flex py-4 bg-[#007448]'>
-      <img src={novoCadastro} alt='receita' className='pr-4'/>
-        <Link to="receita">Receitas</Link>
+      <div className="my-2">
+        <Accordion text="Cadastrados" icon={<PeopleIcon />} options={arr2}/>
       </div>
-      <div>
-        <Accordion text="Cadastrados" image={cadastrados} options={arr2}/>
-      </div>
-      <div id='prontuarios' className='flex py-4 bg-[#007448]'>
-      <img src={prontuarios} alt='prontuarios' className='pr-4'/>
-        <Link to="prontuarios">Prontuários</Link>
+      <div id='prontuarios' className='flex py-4 hover:bg-[#008854] rounded-md px-2 transition-all duration-300'>
+        <FolderSharedIcon className='mr-4' />
+        <Link to="prontuarios" className="w-full">Prontuários</Link>
       </div>
     </div>
   )
